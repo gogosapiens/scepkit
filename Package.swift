7875e94de24e028822156b9d25379f183caf07d8
gogosapiens <gogosapiens@gmail.com>
2025-09-22 16:30:19 +0200

    Updated Package.swift

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.3
+// swift-tools-version:5.9
 import PackageDescription
 
 let package = Package(
@@ -7,31 +7,28 @@ let package = Package(
         .iOS("15.0"),
     ],
     products: [
-        .library(
-            name: "SCEPKit",
-            targets: ["SCEPKit"]
-        ),
+        .library(name: "SCEPKit", targets: ["SCEPKit"]),
     ],
     dependencies: [
-        .package(name: "Amplitude-Swift", url: "https://github.com/amplitude/Amplitude-Swift", .exact("1.11.2")),
-        .package(name: "Adapty", url: "https://github.com/adaptyteam/AdaptySDK-iOS", .exact("3.0.0-beta.2")),
-        .package(name: "Firebase", url: "https://github.com/firebase/firebase-ios-sdk", .exact("10.23.0")),
-        .package(name: "GoogleMobileAds", url: "https://github.com/googleads/swift-package-manager-google-mobile-ads.git", .exact("11.8.0")),
-        .package(name: "SwiftyGif", url: "https://github.com/alexiscreuzot/SwiftyGif", .exact("5.4.5"))
+        .package(url: "https://github.com/amplitude/Amplitude-Swift", from: "1.15.0"),
+        .package(url: "https://github.com/adaptyteam/AdaptySDK-iOS", from: "3.11.0"),
+        .package(url: "https://github.com/firebase/firebase-ios-sdk", from: "12.3.0"),
+        .package(url: "https://github.com/googleads/swift-package-manager-google-mobile-ads.git", from: "12.11.0"),
+        .package(url: "https://github.com/alexiscreuzot/SwiftyGif", from: "5.4.5"),
     ],
     targets: [
         .target(
             name: "SCEPKit",
             dependencies: [
-                .product(name: "AdaptyUI", package: "Adapty"),
-                .product(name: "Adapty", package: "Adapty"),
+                .product(name: "Adapty", package: "AdaptySDK-iOS"),
+                .product(name: "AdaptyUI", package: "AdaptySDK-iOS"),
                 .product(name: "AmplitudeSwift", package: "Amplitude-Swift"),
-                .product(name: "FirebaseRemoteConfig", package: "Firebase"),
-                .product(name: "FirebaseAnalytics", package: "Firebase"),
-                .product(name: "FirebaseAppCheck", package: "Firebase"),
-                .product(name: "FirebaseCrashlytics", package: "Firebase"),
-                .product(name: "GoogleMobileAds", package: "GoogleMobileAds"),
-                .product(name: "SwiftyGif", package: "SwiftyGif")
+                .product(name: "FirebaseRemoteConfig", package: "firebase-ios-sdk"),
+                .product(name: "FirebaseAnalytics", package: "firebase-ios-sdk"),
+                .product(name: "FirebaseAppCheck", package: "firebase-ios-sdk"),
+                .product(name: "FirebaseCrashlytics", package: "firebase-ios-sdk"),
+                .product(name: "GoogleMobileAds", package: "swift-package-manager-google-mobile-ads"),
+                .product(name: "SwiftyGif", package: "SwiftyGif"),
             ],
             resources: [
                 .process("Resources")
