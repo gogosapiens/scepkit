72692b80db6871f36066182f94778abb145f236f
gogosapiens <gogosapiens@gmail.com>
2025-10-22 14:58:07 +0200

    Changed appsflyer code

--- Sources/SCEPKit/Internal/SCEPKitInternal.swift
+++ Sources/SCEPKit/Internal/SCEPKitInternal.swift
@@ -112,7 +112,7 @@ class SCEPKitInternal: NSObject {
         
         if SCEPKitInternal.shared.environment.isUsingProductionProducts && config.legal.enableAppsFlyer == true {
             AppsFlyerLib.shared().appleAppID = "id\(config.integrations.appleAppId)"
-            AppsFlyerLib.shared().appsFlyerDevKey = "nMehdnn5Peag8tic4oXV9W"
+            AppsFlyerLib.shared().appsFlyerDevKey = "skjGkNQxGenPV6uMu7TfKi"
             AppsFlyerLib.shared().delegate = self
             NotificationCenter.default.addOneTimeObserver(forName: UIApplication.didBecomeActiveNotification) { _ in
                 AppsFlyerLib.shared().waitForATTUserAuthorization(timeoutInterval: 10)
